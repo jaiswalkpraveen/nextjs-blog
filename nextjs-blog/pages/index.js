59fff0ee475e8a4920dead1a3037303afb03df14
Praveen <praveenics>
2020-09-02 18:16:53 +0530

    add first code

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>On a QA to SDET journey. I'm reachable at <a href="https://www.linkedin.com/in/jaiswalkpraveen/">LinkedIn</a>.</p>
+
+      </section>
+    </Layout>
+  )
+}
